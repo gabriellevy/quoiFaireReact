f963d95c3f6137898c338d5d71360364e5158b43
gabriellevy <mdeharbe@hotmail.com>
2023-09-12 22:36:38 +0200

    base réactivité

--- src/comp/Saisie.jsx
+++ src/comp/Saisie.jsx
@@ -3,26 +3,37 @@ import { useState, useEffect } from 'react'
 
 
 function Saisie() {
+    const [resultat, setResultat] = useState("???")
     const [dureeMin, setDureeMin] = useState(5)
+
+    function gererResultat() {
+        // refresh calcul de la proposition
+        //1. récupérer valeurs
+
+        //2. comparer avec donnes de lstActivites
+
+        //3. mettre els 3 premiers dans une chaine de caractère et afficher
+        setResultat("je sais aps trop quoi");
+    }
     
     function gererDureeMin(e) {
         setDureeMin(e.target.value)
 
-        // refresh calcul de la proposition
+        gererResultat()
     }
     
     return (
         <div>
+            <div>{resultat}</div>
             <form>
                 <div style={{ padding: '5px 15px 5px 15px' }}>
                 <span>
                     Durée min :
                     <input
                     type="text"
-                    id="age"
+                    id="dureeMin"
                     maxLength={3}
                     pattern="[+-]?\d+(?:[.,]\d+)?"
-                    placeholder="Âge du perso"
                     onChange={gererDureeMin}
                     value={dureeMin}
                     />
